c40cfc177fda175094fbd44b5762e81fdde78d51
Israel Zuñiga de la Mora <israelzdlm@gmail.com>
2011-08-21 22:01:11 -0500

    Actualización web
    Actualizado y editado el sitio web. Corregido en texto: Semestre y fecha

--- web/avances.php
+++ web/avances.php
@@ -19,23 +19,8 @@ include('sidebar.inc');
         <!-- insert the page content here -->
         <h1>Desarrollo y redacci&oacute;n del Proyecto</h1>
         <p>
-	Para acceder a los cap&iacute;tulos del proyecto solo es necesario abrir cualquiera de los siguientes enlaces, que le dirigir&aacute;n al cap&iacute;tulo deseado:  
-	<ul>
-	<li><a href="		https://docs.google.com/viewer?a=v&pid=explorer&chrome=true&srcid=0B1JXJmZz7gBLZDgzYmJlMjEtN2MyMS00ZjAwLWFkMjAtMGIzNDg4NzE4OTY0&hl=en_US
-	">Cap&iacute;tulo 1 - Introducci&oacute;n </a></li>
-
-
-<li>
-<a href="		https://docs.google.com/viewer?a=v&pid=explorer&chrome=true&srcid=0B1JXJmZz7gBLZTViYjg4ZGUtY2M3NS00ZTZmLTkyNDUtNjE4MjUyYjlmMjk2&hl=en_US
-">Cap&iacute;tulo 2 - Estado del Arte </a></li>
-
-<li><a href="https://docs.google.com/viewer?a=v&pid=explorer&chrome=true&srcid=0B1JXJmZz7gBLNTBiMDFhMjYtOTY0YS00OTM3LWI2MTMtZmQxOWU2ZGQ0NWU3&hl=en_US">Cap&iacute;tulo 3 - Metodolog&iacute;a</a></li>
-
-<li><a href="https://docs.google.com/viewer?a=v&pid=explorer&chrome=true&srcid=0B1JXJmZz7gBLMjRlY2Y1ODctYjc3NS00NzkwLWFiZWQtYzk5YTMxYzFmZTBi&hl=en_US">Cap&iacute;tulo 4 - Calendario</a></li>
-
-<li><a href="https://docs.google.com/viewer?a=v&pid=explorer&chrome=true&srcid=0B1JXJmZz7gBLMGI0OTdlM2QtZTkxMy00OTdiLTk3MTItMTk4MjIwOTZkZjc4&hl=en_US">Referencias</a></li>
-
-</ul>
+	Para acceder al texto  del proyecto solo es necesario abrir el siguiente enlace, sin necesidad de usar un lector para PDF:  
+<a href="https://docs.google.com/viewer?a=v&pid=explorer&chrome=true&srcid=0B1JXJmZz7gBLNTIyNjY2OGQtZDhiOC00NTk0LTlhMzEtMWUyNjQ5ZjAxMjEw&hl=en">Tesis al d&iacute;a 22 de Agosto</a> 
 
 </p>
       </div>
